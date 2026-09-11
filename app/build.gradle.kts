6b01f23c5794be8df7cf5d81a1e5b198f5b54746
Harold Inacay <haroldinacay@gmail.com>
2026-09-11 18:39:01 +0800

    Bumped Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.2.1"
         buildConfigField("String", "YOUTUBE_API_KEY", "\"AIzaSyDOuyf7Xt1r5Csck5cCn7-VEiBw8fcbdmI\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
