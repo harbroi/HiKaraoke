42d571db9ff45586fa1689e177138629ba6b9578
Harold Inacay <haroldinacay@gmail.com>
2026-09-11 20:03:04 +0800

    Updated build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2.1"
+        versionName = "1.2.2"
         buildConfigField("String", "YOUTUBE_API_KEY", "\"AIzaSyDOuyf7Xt1r5Csck5cCn7-VEiBw8fcbdmI\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
